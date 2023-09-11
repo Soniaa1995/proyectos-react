0a5adc362d4daccbbc24bc6ab2e03d8180ee4021
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2023-09-11 14:49:52 +0200

    añadiendo map

--- aplicacion-con-vite/following-card-twitter/src/App.jsx
+++ aplicacion-con-vite/following-card-twitter/src/App.jsx
@@ -1,16 +1,38 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+  {
+    userName: 'ejemploAvatar',
+    name: 'Ejemplo Avatar',
+    isFollowing: true
+  },
+  {
+    userName: 'Merenwen151',
+    name: 'Meren',
+    isFollowing: false
+  },
+  {
+    userName: 'DarkMasterZzYT',
+    name: 'DarkMasterZz',
+    isFollowing: true
+  }
+]
+
 export function App () {
   return (
     <section className='App'>
-      <TwitterFollowCard userName="ejemploAvatar" initialIsFollowing={true}>
-        Ejemplo Avatar
-      </TwitterFollowCard>
-
-      <TwitterFollowCard userName="Merenwen151">
-        Meren
-      </TwitterFollowCard>
+      {
+        users.map(({userName, name, isFollowing}) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
     </section>
   )
 }
