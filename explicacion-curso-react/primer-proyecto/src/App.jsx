8e101859db3eb3c22a5cde30832059abb7d4ddbf
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2023-09-14 12:28:03 +0200

    mini actividad para empezar con react

---
+++ explicacion-curso-react/primer-proyecto/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+import { AgregarTarea } from "./components/AgregarTarea"
+
+const Item = ({nombre, visto}) => {
+  return (
+    <li>
+      {nombre}
+      {visto ? '✅' : '⛔'}
+    </li>
+  )
+}
+
+export const App = (props) => {
+
+  const addTask = () => {
+    setArreglo([...arreglo, { nombre: 'nuevo', visto: false}])
+  }
+
+  let listadoSecciones = [
+      {id: 0, nombre: 'ejemplo 1', visto: true},
+      {id: 1, nombre: 'ejemplo 2', visto: true},
+      {id: 2, nombre: 'ejemplo 3', visto: true},
+      {id: 3, nombre: 'ejemplo 4', visto: true},
+      {id: 4, nombre: 'ejemplo 5', visto: true},
+      {id: 5, nombre: 'ejemplo 6', visto: true},
+      {id: 6, nombre: 'ejemplo 7', visto: false},
+      {id: 7, nombre: 'ejemplo 8', visto: false},    
+  ]
+  const [arreglo, setArreglo] = useState(listadoSecciones)
+
+  const onAgregarTarea = (val) => {
+    let valor = val.trim();
+    if(valor < 1) return //si se cumple se sale del metodo
+    const envio = {
+      id: arreglo.length,
+        nombre: valor,
+        visto: false 
+    }
+    setArreglo([...arreglo, envio])
+
+  } 
+
+  return (
+    <>
+      <h1>Ejemplo de listado</h1>
+      <AgregarTarea agregarTarea={onAgregarTarea}></AgregarTarea>
+      <ol>
+        {arreglo.map(item => <Item key={item.id} nombre={item.nombre} visto={item.visto}></Item>)}
+      </ol>
+
+     
+    </>
+  )
+}
+
+
---
+++ explicacion-curso-react/primer-proyecto/src/components/AgregarTarea.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+export const AgregarTarea = ({agregarTarea}) => {
+    const [inputValue, setInputValue] = useState('');
+    const onInputChange = (event) => {
+        setInputValue(event.target.value)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        agregarTarea(inputValue)
+
+    }
+  return (
+    <form onSubmit={onSubmit}>
+        <input 
+            type="text"
+            placeholder="Escribe una tarea nueva"
+            value = {inputValue}
+            onChange={onInputChange} 
+        />
+    </form>
+  )
+}
+
+
